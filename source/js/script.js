143c59bf019ca5cd06800c0440aa0994bb2fe88b
d3x4r <dex3r@mail.ru>
2018-10-11 18:42:40 +0500

    Адаптивная стилизация главной страницы

---
+++ source/js/script.js
@@ -0,0 +1,13 @@
+
+var navMain = document.querySelector('.main-nav');
+var navToggle = document.querySelector('.main-nav__toggle');
+
+navToggle.addEventListener('click', function() {
+  if (navMain.classList.contains('main-nav--close')) {
+    navMain.classList.remove('main-nav--close');
+    navMain.classList.add('main-nav--open');
+  } else {
+    navMain.classList.add('main-nav--close');
+    navMain.classList.remove('main-nav--open');
+  }
+});
