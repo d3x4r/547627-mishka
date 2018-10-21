f0a2d362f952b07d8675970a89f0f8123cf08ac1
d3x4r <dex3r@mail.ru>
2018-10-21 13:57:29 +0500

    Подключение интерактивной карты на главной странице

--- source/js/map.js
+++ source/js/map.js
@@ -1,29 +1,53 @@
-ymaps.ready(init);
+// ymaps.ready(init);
 
-function init () {
-    var myMap = new ymaps.Map(document.getElementsByClassName('contacts__map-yandex')[0], {
-        center:[59.938597,30.323075],
-        zoom: 16.7,
-        controls: []
-    });
+// function init () {
+//     var myMap = new ymaps.Map(document.getElementsByClassName('contacts__map-yandex')[0], {
+//         center:[59.938597,30.323075],
+//         zoom: 16.7,
+//         controls: []
+//     });
 
-    var myGeoObjects = [];
-    myGeoObjects = new ymaps.Placemark([59.938745, 30.323225],{
-                    balloonContentBody: '',
-                    },{
-                    iconImageHref: 'img/icon-map-pin.svg',
-                    iconImageSize: [67, 100],
-                    iconImageOffset: [-35, -80]
-    });
+//     var myGeoObjects = [];
+//     myGeoObjects = new ymaps.Placemark([59.938745, 30.323225],{
+//                     balloonContentBody: '',
+//                     },{
+//                     iconImageHref: 'img/icon-map-pin.svg',
+//                     iconImageSize: [67, 100],
+//                     iconImageOffset: [-35, -80]
+//     });
 
-    var clusterer = new ymaps.Clusterer({
-        clusterDisableClickZoom: false,
-        clusterOpenBalloonOnClick: false,
-    });
+//     var clusterer = new ymaps.Clusterer({
+//         clusterDisableClickZoom: false,
+//         clusterOpenBalloonOnClick: false,
+//     });
 
-    clusterer.add(myGeoObjects);
-    myMap.geoObjects.add(clusterer);
+//     clusterer.add(myGeoObjects);
+//     myMap.geoObjects.add(clusterer);
 
-    myMap.behaviors.disable('scrollZoom');
+//     myMap.behaviors.disable('scrollZoom');
 
-}
+// }
+ymaps.ready(function () {
+  var myMap = new ymaps.Map(document.getElementsByClassName('contacts__map-yandex')[0], {
+          center:[59.938772,30.323197],
+          zoom: 16.7,
+          controls: []
+      }, {
+          searchControlProvider: 'yandex#search'
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+          hintContent: 'Mishka',
+      }, {
+          // Опции.
+          // Необходимо указать данный тип макета.
+          iconLayout: 'default#image',
+          // Своё изображение иконки метки.
+          iconImageHref: 'img/icon-map-pin.svg',
+          // Размеры метки.
+          iconImageSize: [67, 100],
+          iconImageOffset: [-35, -70]
+      })
+  myMap.geoObjects
+      .add(myPlacemark)
+});
