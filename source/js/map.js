70f477bfa9d27d4adac9fe9fe66a1552a5eb6cf9
d3x4r <dex3r@mail.ru>
2018-10-24 11:16:30 +0500

    ...

--- source/js/map.js
+++ source/js/map.js
@@ -1,4 +1,5 @@
-// ymaps.ready(init);
+var myMap;
+//ymaps.ready(init);
 ymaps.ready(function () {
   var myMap = new ymaps.Map(document.getElementsByClassName("contacts__map-yandex")[0], {
           center:[59.938772,30.323197],
