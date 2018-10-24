1825b90e05cd41f535e345258cd293815fab7a0b
d3x4r <dex3r@mail.ru>
2018-10-24 12:31:50 +0500

    ...

--- source/js/map.js
+++ source/js/map.js
@@ -1,7 +1,29 @@
+// var myMap;
+// //ymaps.ready(init);
+// ymaps.ready(function () {
+//   var myMap = new ymaps.Map(document.getElementsByClassName("contacts__map-yandex")[0], {
+//           center:[59.938772,30.323197],
+//           zoom: 16.7,
+//           controls: []
+//       }, {
+//           searchControlProvider: "yandex#search"
+//       }),
+
+//       myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+//           hintContent: "Mishka",
+//       }, {
+//           iconLayout: "default#image",
+//           iconImageHref: "img/icon-map-pin.svg",
+//           iconImageSize: [67, 100],
+//           iconImageOffset: [-35, -70]
+//       })
+//   myMap.geoObjects
+//       .add(myPlacemark)
+// });
 var myMap;
-//ymaps.ready(init);
-ymaps.ready(function () {
-  var myMap = new ymaps.Map(document.getElementsByClassName("contacts__map-yandex")[0], {
+ymaps.ready(init);
+function init () {
+			myMap = new ymaps.Map(document.getElementsByClassName("contacts__map-yandex")[0], {
           center:[59.938772,30.323197],
           zoom: 16.7,
           controls: []
@@ -19,4 +41,4 @@ ymaps.ready(function () {
       })
   myMap.geoObjects
       .add(myPlacemark)
-});
+};
